9a4349c540cce33bc29fbe2b3091d4a55fb375fd
Jimmy Hough <jimmyhoughjr@me.com>
2023-02-24 10:28:10 -0600

    wip

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,10 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.post("postEvents") { req in
+        let bod = "\(req.body)"
+        req.logger.info("\(bod)")
+        return bod
+    }
 }
