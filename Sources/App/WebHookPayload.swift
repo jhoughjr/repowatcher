b03ea7365602c79e32ef9b6617280e1c9180ea2c
Jimmy Hough <jimmyhoughjr@me.com>
2023-02-26 15:22:45 -0600

    Added coding keys to repo

--- Sources/App/WebHookPayload.swift
+++ Sources/App/WebHookPayload.swift
@@ -135,11 +135,95 @@ struct WebHookPayload:Codable  {
    ],*/
     
     struct Repository:Codable {
+       
+        enum CodingKeys: String,CodingKey {
+            case id
+            case node_id
+            case name
+            case full_name
+            case isPrivate = "private"
+            case owner
+            case html_url
+            case description
+            case fork
+            case url
+            case forks_url
+            case keys_url
+            case collaborators_url
+            case teams_url
+            case hooks_url
+            case issue_events_url
+            case events_url
+            case assignees_url
+            case branches_url
+            case tags_url
+            case blobs_url
+            case git_tags_url
+            case git_refs_url
+            case trees_url
+            case statuses_url
+            case languages_url
+            case stargazers_url
+            case contributors_url
+            case subscribers_url
+            case subscription_url
+            case commits_url
+            case git_commits_url
+            case comments_url
+            case issue_comment_url
+            case contents_url
+            case compare_url
+            case merges_url
+            case archive_url
+            case downloads_url
+            case issues_url
+            case pulls_url
+            case milestones_url
+            case notifications_url
+            case labels_url
+            case releases_url
+            case deployments_url
+            case created_at
+            case updated_at
+            case pushed_at
+            case git_url
+            case ssh_url
+            case clone_url
+            case svn_url
+            case homepage
+            case size
+            case stargazers_count
+            case watchers_count
+            case language
+            case has_issues
+            case has_projects
+            case has_downloads
+            case has_wiki
+            case has_pages
+            case has_discussions
+            case forks_count
+            case mirror_url
+            case archived
+            case disabled
+            case open_issues_count
+            case license
+            case allow_forking
+            case is_template
+            case web_commit_signoff_required
+            case topics
+            case visibility
+            case forks
+            case open_issues
+            case watchers
+            case default_branch
+            case stargazers
+            case master_branch
+        }
         var id:Int
         var node_id:String
         var name:String
         var full_name:String
-        var pivate:Bool
+        var isPrivate:Bool
         var owner:Owner
         var html_url: String
         var description: String?
