54cb1905febf3a1a836ba402ffe696db85e0d5b8
Jimmy Hough <jimmyhoughjr@me.com>
2023-03-07 05:57:29 -0600

    wip

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,7 +7,7 @@ public func configure(_ app: Application) throws {
     let file = FileMiddleware(publicDirectory: app.directory.publicDirectory)
     app.middleware.use(file)
     
-    try app.queues.use(.redis(url: "redis://127.0.0.1:6379"))
+    try app.queues.use(.redis(url: "redis://redis:6379"))
     app.http.server.configuration.port = 9090
     
     // register routes
