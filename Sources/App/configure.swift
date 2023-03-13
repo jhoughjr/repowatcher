2e7503c7dbfe3ebcdfbdfb37084fb0f7d9dbca0c
Jimmy Hough <jimmyhoughjr@me.com>
2023-03-13 13:50:42 -0500

    fixed redis url after debug

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -16,7 +16,7 @@ public func configure(_ app: Application) throws {
 //                              path: "",
 //                              logger: app.logger)
     
-    try app.queues.use(.redis(url: "redis://127.0.0.1:6379"))
+    try app.queues.use(.redis(url: "redis://redis:6379"))
     app.queues.add(ScriptJob())
     
     app.logger.info("queues configured/")
